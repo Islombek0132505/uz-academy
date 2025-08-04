b5b7b5b39036aa001a2e7854341c3283390d1728
islombek05052004 <islombek05052004@gmail.com>
2025-08-04 23:52:14 +0500

    added firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAx8jbju1um0uIWfJzMjbT06SpUdJA3CR0",
+  authDomain: "pdp-clone-app.firebaseapp.com",
+  projectId: "pdp-clone-app",
+  storageBucket: "pdp-clone-app.firebasestorage.app",
+  messagingSenderId: "942778331603",
+  appId: "1:942778331603:web:4f6a061cce79983217bf26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
